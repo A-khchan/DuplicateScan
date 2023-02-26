e76df30fb1ae4d5e675719267de442bf874ecfec
Kong Hop Chan <albert88hk@yahoo.com.hk>
2023-02-25 17:20:33 -0800

    Initial Commit

---
+++ DuplicateScan/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DuplicateScan
+//
+//  Created by Albert Chan on 2/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DuplicateScan/DuplicateScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuplicateScanApp.swift
+//  DuplicateScan
+//
+//  Created by Albert Chan on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct DuplicateScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
