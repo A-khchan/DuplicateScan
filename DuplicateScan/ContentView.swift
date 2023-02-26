011e71798ce4cc62fea99c6652484f2a7714db0b
Kong Hop Chan <albert88hk@yahoo.com.hk>
2023-02-25 23:09:01 -0800

    1st version to push

--- DuplicateScan/ContentView.swift
+++ DuplicateScan/ContentView.swift
@@ -8,12 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var SelectedFolder: String = ""
+    @State private var DScanFileManager: FileManager = FileManager()
+    @State private var ContentInDir: [String] = []
+    @State private var FileName: String = ""
+    
     var body: some View {
+        
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            HStack {
+                List(ContentInDir, id: \.self) {
+                    FileName in
+                    Text(FileName)
+            	}
+                List {
+                    
+                }
+            }
+            Picker(selection: .constant(1), label: Text("Folder")) {
+                Text(SelectedFolder).tag(1)
+                //Text("2").tag(2)
+            }
+            Button("Button") {
+                let window: NSWindow = NSApp.mainWindow ?? NSWindow()
+                
+                let panel = NSOpenPanel()
+                panel.canChooseFiles = false
+                panel.canChooseDirectories = true
+                panel.allowsMultipleSelection = false
+                panel.isFloatingPanel = false
+                panel.beginSheetModal(for: window) { (result) in
+                    if result == NSApplication.ModalResponse.OK {
+                        SelectedFolder = panel.urls[0].path
+                        print(panel.urls[0])
+                            
+                        do {
+                            try ContentInDir = DScanFileManager.contentsOfDirectory(atPath: panel.urls[0].path)
+                        } catch {
+                            ContentInDir = []
+                        }
+                        print(ContentInDir)
+                    }
+                }
+            }
+            
         }
         .padding()
     }
