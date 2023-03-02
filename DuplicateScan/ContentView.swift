5c03d4349638ce64513ff75a91800d8161caf52d
Kong Hop Chan <albert88hk@yahoo.com.hk>
2023-03-02 00:03:06 -0800

    Icon to show if having duplication; checkbox for show duplicated file only; summary of duplication (count, size).

--- DuplicateScan/ContentView.swift
+++ DuplicateScan/ContentView.swift
@@ -13,6 +13,7 @@ struct FileWithID: Identifiable {
     var FileName:String
     var FileSize:Int64 = 0
     var FileCreationDate: NSDate?
+    var DupNumber:Int = 0 //0 means no duplication, 1 means first file in duplicated files, duplication means same file size and same creation date.
 }
 
 
@@ -22,45 +23,79 @@ struct ContentView: View {
     @State private var DScanFileManager: FileManager = FileManager()
     @State private var ContentInDir: [String] = []
     @State private var SelectedFile: String? = nil
+    @State private var selectedSize: Int64 = 0
+    @State private var selectedDate: NSDate = Date() as NSDate
+    @State private var SelectedFileForProcess: String? = nil
     @State var listOfFileWithID = [
         FileWithID(FileName: "Empty")
     ]
+    @State var sortedListOfFileWithID: [FileWithID] = []
+    @State private var isOn = false
     
     var body: some View {
         
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
             HStack {
-                
                 VStack {
-                    List(listOfFileWithID, id: \.id, selection: $SelectedFile) { fileWithID in
-                        Text(fileWithID.FileName)
-                            .onTapGesture {
-                                //Store it to adjust color of selected row
-                                self.SelectedFile = fileWithID.FileName
-                                
-                                print("filePath: \(fileWithID.FileName)")
-                                print("filePath: \(fileWithID.FileSize)")
-                                print("filePath: \(String(describing: fileWithID.FileCreationDate))")
-                            }
-                            .listRowBackground(self.SelectedFile == fileWithID.FileName ? Color.blue : Color.white)
-                            .foregroundColor(self.SelectedFile == fileWithID.FileName ? Color.white : Color.black)
+                    Toggle(isOn: $isOn) {
+                        Text("Show only files with duplication")
                     }
+                    .toggleStyle(.checkbox)
+                    
+                    List(sortedListOfFileWithID.filter { $0.DupNumber < 2 && isOn == false ||
+                        $0.DupNumber == 1 && isOn
+                    }, id: \.id, selection: $SelectedFile) { fileWithID in
+                        HStack {
+                            if fileWithID.DupNumber == 0 {
+                                Image(systemName: "doc")
+                                    .imageScale(.large)
+                            } else {
+                                Image(systemName: "doc.on.doc")
+                                    .imageScale(.large)
+                            }
+                            Text(fileWithID.FileName)
+                                .onTapGesture {
+                                    //Store it to adjust color of selected row
+                                    self.SelectedFile = fileWithID.FileName
+                                    self.selectedSize = fileWithID.FileSize
+                                    self.selectedDate = fileWithID.FileCreationDate ?? Date() as NSDate
+                                    
+                                    print("filePath: \(fileWithID.FileName)")
+                                    print("filePath: \(fileWithID.FileSize)")
+                                    print("filePath: \(String(describing: fileWithID.FileCreationDate))")
+                                }
+                                
+                        }
+                        .listRowBackground(self.SelectedFile == fileWithID.FileName ? Color.blue : Color.white)
+                        .foregroundColor(self.SelectedFile == fileWithID.FileName ? Color.white : Color.black)
+                    }
+                    //Summary
+                    Text("Number of file without duplication: \(sortedListOfFileWithID.filter { $0.DupNumber == 0 }.count)")
+                    Text("Number of file with duplication: \(sortedListOfFileWithID.filter { $0.DupNumber == 1 }.count)")
                 }
                 .frame(minWidth: 0, maxWidth: .infinity)
                 
                 VStack {
-                    Text("\(SelectedFile ?? "N/A")")
+                    //Text("\(SelectedFile ?? "N/A")")
+                    List(sortedListOfFileWithID.filter { $0.DupNumber >= 2 && $0.FileSize == selectedSize && $0.FileCreationDate?.timeIntervalSinceReferenceDate == selectedDate.timeIntervalSinceReferenceDate }, id: \.id, selection: $SelectedFileForProcess) { fileWithID in
+                        Text(fileWithID.FileName)
+                            .onTapGesture {
+                                //
+                                self.SelectedFileForProcess = fileWithID.FileName
+                            }
+                            .listRowBackground(self.SelectedFileForProcess == fileWithID.FileName ? Color.blue : Color.white)
+                            .foregroundColor(self.SelectedFileForProcess == fileWithID.FileName ? Color.white : Color.black)
+                    }
+                    Text("Number of duplicated files: \(sortedListOfFileWithID.filter { $0.DupNumber > 1 }.count)")
+                    Text("Total size (in bytes) of duplicated files: \(sortedListOfFileWithID.filter { $0.DupNumber > 1 }.reduce(0, { $0 + $1.FileSize}))")
+                    
                 }
                 .frame(minWidth: 0, maxWidth: .infinity)
             }
             Picker(selection: .constant(1), label: Text("Folder")) {
                 Text(SelectedFolder ).tag(1)
             }
-            Button("Button") {
+            Button("Choose folder") {
                 let window: NSWindow = NSApp.mainWindow ?? NSWindow()
                 
                 let panel = NSOpenPanel()
@@ -96,6 +131,49 @@ struct ContentView: View {
                         print(ContentInDir)
                         
                         listOfFileWithID = getFileInfoArray(folder: panel.urls[0].path)
+                        sortedListOfFileWithID = listOfFileWithID.sorted(by: ) { (lhs, rhs) in
+                            if lhs.FileCreationDate == rhs.FileCreationDate {
+                                return lhs.FileSize < rhs.FileSize
+                            }
+                            
+                            let L = lhs.FileCreationDate ?? Date() as NSDate
+                            let R = rhs.FileCreationDate ?? Date() as NSDate
+                            
+                            return L.timeIntervalSinceReferenceDate
+                            < R.timeIntervalSinceReferenceDate
+                            
+                        }
+                        
+                        var prevSize:Int64 = 0
+                        var prevDate:NSDate = Date() as NSDate
+                        var i = 0
+                        for item in sortedListOfFileWithID {
+                            //print("Name: \(item.FileName)")
+                            //print("Size: \(item.FileSize) ")
+                            //print("Create Date: \(String(describing: item.FileCreationDate))")
+                            if i > 0 {
+                                let thisDate = item.FileCreationDate ?? Date() as NSDate
+                                if prevSize == item.FileSize &&
+                                    prevDate.timeIntervalSinceReferenceDate == thisDate.timeIntervalSinceReferenceDate {
+                                    if sortedListOfFileWithID[i-1].DupNumber == 0 {
+                                        sortedListOfFileWithID[i-1].DupNumber = 1
+                                    }
+                                    sortedListOfFileWithID[i].DupNumber = sortedListOfFileWithID[i-1].DupNumber + 1
+                                }
+                            }
+                            prevSize = item.FileSize
+                            prevDate = item.FileCreationDate ?? Date() as NSDate
+                            i += 1
+                        }
+                        
+                        print("****Sorted List is:")
+                        for item in sortedListOfFileWithID {
+                            print("Name: \(item.FileName)")
+                            print("Size: \(item.FileSize) ")
+                            print("Create Date: \(String(describing: item.FileCreationDate))")
+                            print("DupNumber: \(item.DupNumber)")
+                        }
+                        //print(sortedListOfFileWithID)
                     }
                 }
             }
